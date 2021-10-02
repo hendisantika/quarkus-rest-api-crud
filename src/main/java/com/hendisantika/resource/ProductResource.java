aa620396b50f6d9309fb99f0e40298e46516a6f5
Hendi Santika <hendisantika@gmail.com>
2021-10-02 07:20:08 +0700

    #10 - Add ProductResource

---
+++ src/main/java/com/hendisantika/resource/ProductResource.java
@@ -0,0 +1,16 @@
+package com.hendisantika.resource;
+
+import javax.ws.rs.Path;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : quarkus-rest-api-crud
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 02/10/21
+ * Time: 07.19
+ */
+@Path("/v1/products")
+public class ProductResource {
+}
