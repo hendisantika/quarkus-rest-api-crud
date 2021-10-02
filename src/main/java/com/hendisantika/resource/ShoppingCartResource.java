429a393e6930be6dc19e91e8653dddaa49a666ae
Hendi Santika <hendisantika@gmail.com>
2021-10-02 07:28:51 +0700

    #18 - Add getSingleCart API in ShoppingCartResource

--- src/main/java/com/hendisantika/resource/ShoppingCartResource.java
+++ src/main/java/com/hendisantika/resource/ShoppingCartResource.java
@@ -5,9 +5,12 @@ import io.smallrye.mutiny.Uni;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import static javax.ws.rs.core.Response.Status.NOT_FOUND;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,5 +29,15 @@ public class ShoppingCartResource {
         return ShoppingCart.getAllShoppingCarts()
                 .onItem().transform(shoppingcarts -> Response.ok(shoppingcarts))
                 .onItem().transform(Response.ResponseBuilder::build);
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Uni<Response> getSingleCart(@PathParam("id") Long id) {
+        return ShoppingCart.findByShoppingCartId(id)
+                .onItem().ifNotNull().transform(cart -> Response.ok(cart).build())
+                .onItem().ifNull().continueWith(Response.ok().status(NOT_FOUND)::build);
+
     }
 }
