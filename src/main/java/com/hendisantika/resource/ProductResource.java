66fed668cea86bf4b713a4877d38a901043ffa46
Hendi Santika <hendisantika@gmail.com>
2021-10-02 07:21:23 +0700

    #11 - Add getProducts API in ProductResource

--- src/main/java/com/hendisantika/resource/ProductResource.java
+++ src/main/java/com/hendisantika/resource/ProductResource.java
@@ -1,6 +1,13 @@
 package com.hendisantika.resource;
 
+import com.hendisantika.entity.Product;
+import io.smallrye.mutiny.Uni;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * Created by IntelliJ IDEA.
@@ -13,4 +20,11 @@ import javax.ws.rs.Path;
  */
 @Path("/v1/products")
 public class ProductResource {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Uni<Response> getProducts() {
+        return Product.getAllProducts()
+                .onItem().transform(products -> Response.ok(products))
+                .onItem().transform(Response.ResponseBuilder::build);
+    }
 }
