f0839871c5f8b65de0c27a40c334c2e871120c65
Hendi Santika <hendisantika@gmail.com>
2021-10-02 07:22:27 +0700

    #12 - Add getSingleProduct API in ProductResource

--- src/main/java/com/hendisantika/resource/ProductResource.java
+++ src/main/java/com/hendisantika/resource/ProductResource.java
@@ -5,9 +5,12 @@ import io.smallrye.mutiny.Uni;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import static javax.ws.rs.core.Response.Status.NOT_FOUND;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,5 +29,13 @@ public class ProductResource {
         return Product.getAllProducts()
                 .onItem().transform(products -> Response.ok(products))
                 .onItem().transform(Response.ResponseBuilder::build);
+    }
+
+    @GET
+    @Path("{id}")
+    public Uni<Response> getSingleProduct(@PathParam("id") Long id) {
+        return Product.findByProductId(id)
+                .onItem().ifNotNull().transform(product -> Response.ok(product).build())
+                .onItem().ifNull().continueWith(Response.ok().status(NOT_FOUND)::build);
     }
 }
