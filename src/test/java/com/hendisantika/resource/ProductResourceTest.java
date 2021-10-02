8804a9695213ee7cb3cbd5790ea8da23e0cab8b0
Hendi Santika <hendisantika@gmail.com>
2021-10-02 08:17:37 +0700

    #23 - Add testCreateProduct Test Case in ProductResourceTest

--- src/test/java/com/hendisantika/resource/ProductResourceTest.java
+++ src/test/java/com/hendisantika/resource/ProductResourceTest.java
@@ -1,10 +1,15 @@
 package com.hendisantika.resource;
 
+import com.hendisantika.entity.Product;
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
 import javax.json.bind.JsonbConfig;
+
+import static io.restassured.RestAssured.given;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,4 +23,21 @@ import javax.json.bind.JsonbConfig;
 @QuarkusTest
 class ProductResourceTest {
     static final Jsonb jsonb = JsonbBuilder.create(new JsonbConfig());
+
+    @Test
+    @Order(1)
+    public void testCreateProduct() {
+        Product product = Product
+                .builder()
+                .title("product_title")
+                .description("product_description")
+                .build();
+        given()
+                .when()
+                .header("Content-Type", "application/json")
+                .body(jsonb.toJson(product))
+                .post("/v1/products")
+                .then()
+                .statusCode(201);
+    }
 }
