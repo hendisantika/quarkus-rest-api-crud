fe27e431058066b5c864ffe07a2d535d27aed842
Hendi Santika <hendisantika@gmail.com>
2021-10-02 07:27:19 +0700

    #17 - Add getCarts API in ShoppingCartResource

--- src/main/java/com/hendisantika/resource/ShoppingCartResource.java
+++ src/main/java/com/hendisantika/resource/ShoppingCartResource.java
@@ -1,6 +1,13 @@
 package com.hendisantika.resource;
 
+import com.hendisantika.entity.ShoppingCart;
+import io.smallrye.mutiny.Uni;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * Created by IntelliJ IDEA.
@@ -13,4 +20,11 @@ import javax.ws.rs.Path;
  */
 @Path("/v1/carts")
 public class ShoppingCartResource {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Uni<Response> getCarts() {
+        return ShoppingCart.getAllShoppingCarts()
+                .onItem().transform(shoppingcarts -> Response.ok(shoppingcarts))
+                .onItem().transform(Response.ResponseBuilder::build);
+    }
 }
