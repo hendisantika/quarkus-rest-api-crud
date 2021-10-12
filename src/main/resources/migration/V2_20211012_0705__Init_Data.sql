21d1b91fc9e77602f0ef7af17ee1bbc06578bfac
Hendi Santika <hendisantika@gmail.com>
2021-10-12 13:57:15 +0700

    #31 - Add Flyway migration scripts

---
+++ src/main/resources/migration/V2_20211012_0705__Init_Data.sql
@@ -0,0 +1,25 @@
+INSERT
+INTO product
+    (title, description, createdAt, updatedAt)
+VALUES ('Product1', 'description', NOW(), NOW());
+
+INSERT
+INTO product
+    (title, description, createdAt, updatedAt)
+VALUES ('Product2', 'description', NOW(), NOW())
+;
+
+INSERT
+INTO product
+    (title, description, createdAt, updatedAt)
+VALUES ('Product3', 'description', NOW(), NOW());
+
+INSERT
+INTO product
+    (title, description, createdAt, updatedAt)
+VALUES ('Product4', 'description', NOW(), NOW());
+
+INSERT
+INTO shoppingcart
+    (cartTotal, name)
+VALUES (0, 'MyCart');
