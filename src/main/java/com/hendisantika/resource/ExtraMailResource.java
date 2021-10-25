a0b0e2d720c64f4c8f072a251edba888d3add2aa
Hendi Santika <hendisantika@gmail.com>
2021-10-25 14:25:10 +0700

    #43 - Add sendTypeSafeTemplate API in ExtraMailResource

--- src/main/java/com/hendisantika/resource/ExtraMailResource.java
+++ src/main/java/com/hendisantika/resource/ExtraMailResource.java
@@ -5,6 +5,7 @@ import io.quarkus.mailer.MailTemplate;
 import io.quarkus.mailer.Mailer;
 import io.quarkus.qute.CheckedTemplate;
 import io.smallrye.common.annotation.Blocking;
+import io.smallrye.mutiny.Uni;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -41,4 +42,13 @@ public class ExtraMailResource {
         public static native MailTemplate.MailTemplateInstance hello(String name); // <1>
     }
 
+    @GET
+    @Path("/template")
+    public Uni<Void> sendTypeSafeTemplate() {
+        // the template looks like: Hello {name}!
+        return Templates.hello("John")
+                .to("your-destination-email@quarkus.io")
+                .subject("Hello from Qute template")
+                .send();
+    }
 }
