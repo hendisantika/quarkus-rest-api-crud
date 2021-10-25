95dbe58366437b522207af049246bfc93cfc5177
Hendi Santika <hendisantika@gmail.com>
2021-10-25 14:26:05 +0700

    #45 - Add TypeSafeMailResource

---
+++ src/main/java/com/hendisantika/resource/TypeSafeMailResource.java
@@ -0,0 +1,16 @@
+package com.hendisantika.resource;
+
+import javax.ws.rs.Path;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : quarkus-rest-api-crud
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 25/10/21
+ * Time: 14.25
+ */
+@Path("/type-safe")
+public class TypeSafeMailResource {
+}
