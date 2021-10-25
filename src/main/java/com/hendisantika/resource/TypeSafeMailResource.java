a7429019334af8bbae709b048aa6611f9ef33772
Hendi Santika <hendisantika@gmail.com>
2021-10-25 14:26:38 +0700

    #46 - Add greeting API in TypeSafeMailResource

--- src/main/java/com/hendisantika/resource/TypeSafeMailResource.java
+++ src/main/java/com/hendisantika/resource/TypeSafeMailResource.java
@@ -1,6 +1,18 @@
 package com.hendisantika.resource;
 
+import io.quarkus.mailer.MailTemplate;
+import io.quarkus.qute.CheckedTemplate;
+import io.smallrye.mutiny.Uni;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * Created by IntelliJ IDEA.
@@ -13,4 +25,20 @@ import javax.ws.rs.Path;
  */
 @Path("/type-safe")
 public class TypeSafeMailResource {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Uni<Response> greeting(
+            @Valid @Email @QueryParam("email") String email,
+            @Valid @NotBlank @QueryParam("name") String name) {
+        return Templates.hello(name)
+                .to(email)
+                .subject("Ahoy " + name + "!")
+                .send()
+                .map(x -> Response.accepted().build());
+    }
+
+    @CheckedTemplate
+    static class Templates {
+        public static native MailTemplate.MailTemplateInstance hello(String name);
+    }
 }
