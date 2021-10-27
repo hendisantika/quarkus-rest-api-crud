66c5638a73e371927de7bc0d6cf8b297ece5817f
Hendi Santika <hendisantika@gmail.com>
2021-10-27 16:46:11 +0700

    #64 - Add MailTest

--- src/test/java/com/hendisantika/MailTest.java
+++ src/test/java/com/hendisantika/MailTest.java
@@ -1,10 +1,19 @@
 package com.hendisantika;
 
+import io.quarkus.mailer.Mail;
 import io.quarkus.mailer.MockMailbox;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.hamcrest.Matchers.is;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,5 +35,25 @@ class MailTest {
     @BeforeEach
     void init() {
         mailbox.clear();
+    }
+
+    @Test
+    void testTextMail() throws MessagingException, IOException {
+        // call a REST endpoint that sends email
+        given()
+                .when()
+                .get("/mail")
+                .then()
+                .statusCode(202)
+                .body(is("OK"));
+
+        // verify that it was sent
+        List<Mail> sent = mailbox.getMessagesSentTo(TO);
+        assertThat(sent).hasSize(1);
+        Mail actual = sent.get(0);
+        assertThat(actual.getText()).contains("Wake up!");
+        assertThat(actual.getSubject()).isEqualTo("Alarm!");
+
+        assertThat(mailbox.getTotalMessagesSent()).isEqualTo(6);
     }
 }
