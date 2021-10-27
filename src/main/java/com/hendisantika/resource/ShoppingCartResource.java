4c31003d88e1838975e6767e3434da35c19c2194
Hendi Santika <hendisantika@gmail.com>
2021-10-27 14:15:13 +0700

    #57 - Modify ShoppingCartResource

--- src/main/java/com/hendisantika/resource/ShoppingCartResource.java
+++ src/main/java/com/hendisantika/resource/ShoppingCartResource.java
@@ -1,14 +1,16 @@
 package com.hendisantika.resource;
 
+import com.hendisantika.entity.Product;
 import com.hendisantika.entity.ShoppingCart;
-import io.smallrye.mutiny.Uni;
+import com.hendisantika.repository.ProductRepository;
+import com.hendisantika.repository.ShoppingCartRepository;
 
+import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.net.URI;
-
-import static javax.ws.rs.core.Response.Status.NOT_FOUND;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,52 +23,57 @@ import static javax.ws.rs.core.Response.Status.NOT_FOUND;
  */
 @Path("/v1/carts")
 public class ShoppingCartResource {
+    @Inject
+    ShoppingCartRepository shoppingCartRepository;
+
+    @Inject
+    ProductRepository productRepository;
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Uni<Response> getCarts() {
-        return ShoppingCart.getAllShoppingCarts()
-                .onItem().transform(shoppingcarts -> Response.ok(shoppingcarts))
-                .onItem().transform(Response.ResponseBuilder::build);
+    public List<ShoppingCart> getCarts() {
+        return ShoppingCart.listAll();
     }
 
     @GET
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Uni<Response> getSingleCart(@PathParam("id") Long id) {
-        return ShoppingCart.findByShoppingCartId(id)
-                .onItem().ifNotNull().transform(cart -> Response.ok(cart).build())
-                .onItem().ifNull().continueWith(Response.ok().status(NOT_FOUND)::build);
-
+    public ShoppingCart getSingleCart(@PathParam("id") Long id) {
+        return ShoppingCart.findById(id);
     }
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Uni<Response> createShoppingCart(ShoppingCart shoppingCart) {
+    public Response createShoppingCart(ShoppingCart shoppingCart) {
         if (shoppingCart == null || shoppingCart.name == null) {
             throw new WebApplicationException("ShoppingCart name was not set on request.", 422);
         }
-        return ShoppingCart.createShoppingCart(shoppingCart)
-                .onItem().transform(id -> URI.create("/v1/carts/" + id.id))
-                .onItem().transform(uri -> Response.created(uri))
-                .onItem().transform(Response.ResponseBuilder::build);
+        shoppingCartRepository.persist(shoppingCart);
+        return Response.created(URI.create("/carts/" + shoppingCart.id)).build();
     }
 
     @PUT
     @Path("{cartid}/{productid}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Uni<Response> update(@PathParam("cartid") Long id, @PathParam("productid") Long product) {
-        return ShoppingCart.addProductToShoppingCart(id, product)
-                .onItem().ifNotNull().transform(entity -> Response.ok(entity).build())
-                .onItem().ifNull().continueWith(Response.ok().status(NOT_FOUND)::build);
+    public ShoppingCart update(@PathParam("cartid") Long id, @PathParam("productid") Long product) {
+        ShoppingCart result = shoppingCartRepository.findById(id);
+        Product product1 = productRepository.findById(product);
+
+        if (result == null || product1 == null) {
+            throw new WebApplicationException("Shopping Cart or Product description was not set on request.", 422);
+        }
+        return result;
     }
 
     @DELETE
     @Path("{cartid}/{productid}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Uni<Response> delete(@PathParam("cartid") Long id, @PathParam("productid") Long product) {
-        return ShoppingCart.deleteProductFromShoppingCart(id, product)
-                .onItem().ifNotNull().transform(entity -> Response.ok(entity).build())
-                .onItem().ifNull().continueWith(Response.ok().status(NOT_FOUND)::build);
+    public void delete(@PathParam("cartid") Long id, @PathParam("productid") Long product) {
+        ShoppingCart result = shoppingCartRepository.findById(id);
+        if (result == null) {
+            throw new WebApplicationException("Product ID was not set on request.", 422);
+        }
+        result.delete();
     }
 }
